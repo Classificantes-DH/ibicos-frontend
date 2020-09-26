dbb8d12bc167bc2849021ae2815b506f7d977f16
siqueira-ec <siqueira-ec@users.noreply.github.com>
2020-09-26 19:01:10 +0000

    deploy: 255562ff8603eae8d85a0ccccca816cbaea3dc86

---
+++ precache-manifest.598d98113092f784b5f6b879b98ad9e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beedf859d213c01f0b8705efc4af0bb9",
+    "url": "/ibicos-frontend/index.html"
+  },
+  {
+    "revision": "05de089778dd91161c69",
+    "url": "/ibicos-frontend/static/css/main.4acd088a.chunk.css"
+  },
+  {
+    "revision": "e3b9f1e519a6b2bbf32f",
+    "url": "/ibicos-frontend/static/js/2.6f54c5ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ibicos-frontend/static/js/2.6f54c5ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05de089778dd91161c69",
+    "url": "/ibicos-frontend/static/js/main.db8cc1c0.chunk.js"
+  },
+  {
+    "revision": "797e4f511b2affdbf61c",
+    "url": "/ibicos-frontend/static/js/runtime-main.bb320370.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ibicos-frontend/precache-manifest.598d98113092f784b5f6b879b98ad9e4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ibicos-frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
