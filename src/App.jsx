291433b146fb23b2f7c47e17006702d27a6ed219
Matheus Siqueira <matheussiqueira.ec@gmail.com>
2020-09-28 14:05:00 -0300

    refactor(base project): added commit hooks
    pre-commit hooks to execute linting on code and commit message before commit; jsx only on .jsx rule;

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Home, ClassifiedAdsList } from "./pages";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <>
         <Layout>
           <Switch>
