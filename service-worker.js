1f15afbf952503e7e599fd8b72de1a3f1360adff
siqueira-ec <siqueira-ec@users.noreply.github.com>
2020-09-28 17:59:21 +0000

    deploy: 291433b146fb23b2f7c47e17006702d27a6ed219

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ibicos-frontend/precache-manifest.3297c6a2a9f1ca871af2c35a9b4a6268.js"
+  "/ibicos-frontend/precache-manifest.cbb07e6b25fd361f62ed51c3edec1b21.js"
 );
 
 self.addEventListener('message', (event) => {
