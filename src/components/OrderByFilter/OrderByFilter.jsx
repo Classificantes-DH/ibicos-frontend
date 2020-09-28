91427af7ff8ad63506df4927f9f0ed002454560b
Maik Henrique <maik.hsp.contato@gmail.com>
2020-09-28 08:36:47 -0300

    feat - Implemented Order By filter

--- src/components/OrderByFilter/OrderByFilter.jsx
+++ src/components/OrderByFilter/OrderByFilter.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import styles from "./OrderByFilter.module.scss";
 
-const OrderByFilter = () => <p>EAEA</p>;
+const OrderByFilter = () => (
+  <div className={styles.container}>
+    <div className={styles.adsFoundContainer}>
+      <p>
+        Anúncios: <span>123</span>
+      </p>
+    </div>
+    <div className={styles.orderByContainer}>
+      <p>Ordenar por:</p>
+      <select>
+        <option>Qualquer</option>
+        <option>Melhores Avaliados</option>
+        <option>Mais Avaliados</option>
+      </select>
+    </div>
+  </div>
+);
 
 export default OrderByFilter;
