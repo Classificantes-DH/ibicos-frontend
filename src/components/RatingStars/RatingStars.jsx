310e38eb3ffe0171d75add881a0e80f9b74b7994
Maik Henrique <maik.hsp.contato@gmail.com>
2020-10-02 07:45:09 -0300

    fix(base structure): Fix of merging conflict issues
    Fix of merging clonfict issues by accepting changes.

--- src/components/RatingStars/RatingStars.jsx
+++ src/components/RatingStars/RatingStars.jsx
@@ -6,11 +6,7 @@ const RatingStars = () => {
     <ReactStars
       caption="Número de avaliações"
       count={5}
-<<<<<<< HEAD
       size={30}
-=======
-      size={20}
->>>>>>> 291433b146fb23b2f7c47e17006702d27a6ed219
       color="white"
       activeColor="#5b074d"
       edit={false}
--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,4 @@ export { default as Header } from "./Header/Header";
 export { default as JobCard } from "./JobCard/JobCard";
 export { default as RatingStars } from "./RatingStars/RatingStars";
 export { default as OrderByFilter } from "./OrderByFilter/OrderByFilter";
-<<<<<<< HEAD
 export { default as BroadFilter } from "./BroadFilter/BroadFilter";
-=======
->>>>>>> 291433b146fb23b2f7c47e17006702d27a6ed219
