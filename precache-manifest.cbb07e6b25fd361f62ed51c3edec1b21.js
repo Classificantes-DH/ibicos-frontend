7ef0de4c0b706713331ae56a2b5e5e5e5d957f3a
siqueira-ec <siqueira-ec@users.noreply.github.com>
2020-10-05 12:48:16 +0000

    deploy: 3492fa8e5ea46db756ea137ca617df3dbbc911b0

---
+++ precache-manifest.cbb07e6b25fd361f62ed51c3edec1b21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f83f3555e17c5b4a173a999e7f09699",
+    "url": "/ibicos-frontend/index.html"
+  },
+  {
+    "revision": "9a6f49a20c375cbdaf95",
+    "url": "/ibicos-frontend/static/css/main.4c115738.chunk.css"
+  },
+  {
+    "revision": "545da1cbfe943001a6d4",
+    "url": "/ibicos-frontend/static/js/2.05917815.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ibicos-frontend/static/js/2.05917815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6f49a20c375cbdaf95",
+    "url": "/ibicos-frontend/static/js/main.3c06227d.chunk.js"
+  },
+  {
+    "revision": "797e4f511b2affdbf61c",
+    "url": "/ibicos-frontend/static/js/runtime-main.bb320370.js"
+  },
+  {
+    "revision": "641bc0922db3cca5c2147c0865921301",
+    "url": "/ibicos-frontend/static/media/map-location.641bc092.svg"
+  },
+  {
+    "revision": "86a06f7a9984876348cfd19e55aed40b",
+    "url": "/ibicos-frontend/static/media/plumber.86a06f7a.svg"
+  }
+]);
