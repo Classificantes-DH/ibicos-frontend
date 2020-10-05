6ec565bace7f5024a31f74745b758f71e0541062
Matheus Siqueira <matheussiqueira.ec@gmail.com>
2020-10-04 21:36:13 -0300

    refactor(signup successful page): add dynamic bg depending of parent route

--- src/components/FormSignupSuccessful/FormSignupSuccessful.jsx
+++ src/components/FormSignupSuccessful/FormSignupSuccessful.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 import styles from "./FormSignupSuccessful.module.scss";
 
 import successfulSignupImage from "../../resources/imgs/styleSvgs/signup_success.svg";
 
 const FormSignupSuccessful = () => {
+  const location = useLocation();
+
+  const { signupEntityType } = location.state
+    ? location.state
+    : { signupEntityType: undefined };
+
+  const bgStyle = {};
+
+  bgStyle.backgroundColor =
+    signupEntityType && signupEntityType === "client"
+      ? styles.purpleBg
+      : styles.orangeBg;
+
   return (
     <>
-      <div className={styles.container}>
+      <div className={styles.container} style={bgStyle}>
         <div className={styles.successIndicator}>
           <div className={styles.successBanner}>
             <p>Cadastro realizado</p>
