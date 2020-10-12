a094c3e5a484ce2d563af820b891463fc6688af6
Maik Henrique <maik.hsp.contato@gmail.com>
2020-10-11 21:28:07 -0300

    feat(form modal): implementation of basic stylization
    - implementation of basic stylization for the starting point of the screens.

--- src/components/JobCard/ModalHolder/FormModal/FormModal.jsx
+++ src/components/JobCard/ModalHolder/FormModal/FormModal.jsx
@@ -7,31 +7,40 @@ import emailIcon from "../../../../resources/imgs/utilityIcons/email.svg";
 
 const FormModal = ({ handleModalClose, handleFormSent }) => {
   return (
-    <div className={`${styles.contentContainer} container`}>
-      <button onClick={handleModalClose} type="button">
-        Click to close
-      </button>
-      <p>
-        <strong>Se interessou?</strong> Entre em contato agora mesmo com o
-        profissional!
-      </p>
-      <form
-        onSubmit={() => {
-          handleModalClose();
-          handleFormSent();
-        }}
-      >
-        <textarea>Insira sua mensagem</textarea>
-        <button type="submit">Enviar</button>
-      </form>
-      <h2>Ou se preferir...</h2>
-      <div className={styles.contact}>
-        <img src={whatsAppIcon} alt="Contacte por telefone/whatsapp" />
-        <p>11 123445678</p>
-      </div>
-      <div className={styles.contact}>
-        <img src={emailIcon} alt="Contacte por email" />
-        <p>email.prestador@email.com</p>
+    <div className={`${styles.container} container`}>
+      <div className={styles.contentContainer}>
+        <div className={styles.btnContainer}>
+          <button onClick={handleModalClose} type="button">
+            X
+          </button>
+        </div>
+        <div className={styles.introContent}>
+          <p>
+            <strong>Se interessou?</strong> Entre em contato agora mesmo com o
+            profissional!
+          </p>
+        </div>
+        <form
+          className={styles.formContainer}
+          onSubmit={() => {
+            handleModalClose();
+            handleFormSent();
+          }}
+        >
+          <textarea placeholder="Insira sua mensagem" />
+          <button type="submit">Enviar</button>
+        </form>
+        <div className={styles.contactInfoContainer}>
+          <h2>Ou se preferir...</h2>
+          <div className={styles.contact}>
+            <img src={whatsAppIcon} alt="Contacte por telefone/whatsapp" />
+            <p>11 123445678</p>
+          </div>
+          <div className={styles.contact}>
+            <img src={emailIcon} alt="Contacte por email" />
+            <p>email.prestador@email.com</p>
+          </div>
+        </div>
       </div>
     </div>
   );
