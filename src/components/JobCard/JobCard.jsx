b1f2dcd134c56a87766c60e6a19838e9d67064be
Maik Henrique <maik.hsp.contato@gmail.com>
2020-10-18 20:34:11 -0300

    feat(job card): implementation of dynamic rendering
    - implementation of dynamic rendering using props.
    - refactor of list of regions in html structure and stylization.
    - change of rating start to be rendered dynamically through props.
    - change of the mocked json to include additional variation.

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
+import PropTypes from "prop-types";
 import styles from "./JobCard.module.scss";
 
 import mechanicLogo from "../../resources/imgs/jobIcons/plumber.svg";
 import mapIcon from "../../resources/imgs/utilityIcons/map-location.svg";
 import { RatingStars } from "../index";
 
-const JobCard = () => {
+const JobCard = ({ adInfo }) => {
+  const { category, cities, description, evaluations, rate, state } = adInfo;
+
   return (
     <>
       <div className={styles.container}>
@@ -14,9 +17,13 @@ const JobCard = () => {
           <img src={mechanicLogo} alt="Job Logo" className={styles.jobLogo} />
         </div>
         <div className={styles.mainDescription}>
-          <h2 className={styles.title}>Mecânico</h2>
-          <RatingStars />
-          <p>3 avaliações</p>
+          <h2 className={styles.title}>{category}</h2>
+          <RatingStars
+            rate={rate}
+            isEditable={false}
+            description="Quantidade de avaliações"
+          />
+          <p>{evaluations} avaliações</p>
         </div>
         <div className={styles.jobDetails}>
           <div className={styles.location}>
@@ -29,7 +36,13 @@ const JobCard = () => {
                 />
                 <h4>Locais</h4>
               </div>
-              <p>São Paulo | SP</p>
+              <ul className={styles.locationList}>
+                {cities.map(({ name }) => (
+                  <li key={name}>
+                    {name} | {state}
+                  </li>
+                ))}
+              </ul>
             </div>
             <div className={styles.locationSpecific}>
               <div className={styles.locationSpecificRegion}>
@@ -40,21 +53,42 @@ const JobCard = () => {
                 />
                 <h4>Região</h4>
               </div>
-              <p>Zona Sul, Zona Leste</p>
+              <ul className={styles.locationList}>
+                {cities.map(({ name, regions }) =>
+                  regions.map((region) => (
+                    <li key={region}>
+                      {region} | {name}
+                    </li>
+                  ))
+                )}
+              </ul>
             </div>
           </div>
           <div className={styles.description}>
             <h4>Descrição</h4>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt
-              quidem explicabo animi dolores distinctio vitae amet quo suscipit
-              facere nihil?
-            </p>
+            <p>{description}</p>
           </div>
         </div>
       </div>
     </>
   );
+};
+
+JobCard.propTypes = {
+  adInfo: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    category: PropTypes.string.isRequired,
+    evaluations: PropTypes.number.isRequired,
+    state: PropTypes.string.isRequired,
+    rate: PropTypes.number.isRequired,
+    cities: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        region: PropTypes.arrayOf(PropTypes.string),
+      })
+    ).isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default JobCard;
