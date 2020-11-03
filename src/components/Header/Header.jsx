59b51532ca820b8a6d2b72726f66c3da58e9e02c
Maik Henrique <maik.hsp.contato@gmail.com>
2020-11-03 13:35:53 -0300

    feat(header): implementation of basic structure
    - implementation of basic header structure
    - basic stylization
    - addition of burger button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.scss";
+import {
+  companyLogo,
+  searchIcon,
+} from "../../resources/imgs/utilityIcons/index";
 
 const Header = () => {
-  return <p className={styles.testes}>Oi sou o Header</p>;
+  const [isBurgerActive, setIsBurgerActive] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsBurgerActive(!isBurgerActive);
+  };
+  return (
+    <header className={styles.container}>
+      <div className={styles.companyLogo}>
+        <img src={companyLogo} alt="Logo da empresa iBicos" />
+        <h4>iBicos</h4>
+      </div>
+      <div className={styles.searchAd}>
+        <input type="text" placeholder="Pesquise nossos anunciantes!" />
+        <button className={styles.searchIconContainer} type="button">
+          <img src={searchIcon} alt="Ícone da lupa" />
+        </button>
+      </div>
+      <div
+        className={styles.burgerMenu}
+        onClick={handleMenuToggle}
+        role="button"
+        onKeyPress={handleMenuToggle}
+        tabIndex={0}
+      >
+        <button
+          type="button"
+          className={`${styles.burgerMenu} ${
+            isBurgerActive ? `${styles.burgerActive}` : ``
+          }`}
+        >
+          <div className={styles.burgerLine} />
+          <div className={styles.burgerLine} />
+          <div className={styles.burgerLine} />
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/resources/imgs/utilityIcons/index.js
+++ src/resources/imgs/utilityIcons/index.js
@@ -6,4 +6,5 @@ export { default as twitterIcon } from "./socialMedia/twitter.svg";
 export { default as phoneIcon } from "./phone.svg";
 export { default as emailIcon } from "./email.svg";
 export { default as locationIcon } from "./map-location.svg";
+export { default as searchIcon } from "./search-icon.svg";
 export { default as companyLogo } from "./company-logo.svg";
