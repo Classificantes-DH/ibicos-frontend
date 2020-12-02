425cb391b1ad9ecd0af5df1c3856b91ee25dc28b
Maik Henrique <maik.hsp.contato@gmail.com>
2020-12-02 20:04:13 -0300

    feat(loading spinner): implementation of the loading spinner component
    - implementation of the stylization of the loading spinner
    - implementation of the default component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { LoadingSpinner } from "../../components/index";
 
 const Home = () => {
-  return <p>Oi sou a Home</p>;
+  return <LoadingSpinner />;
 };
 
 export default Home;
