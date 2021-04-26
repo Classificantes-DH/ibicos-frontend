367c68cffd4ae28528123a8c588882d9e845aed4
Maik Henrique <maik.hsp.contato@gmail.com>
2021-04-26 18:13:53 -0300

    feat(hooks): creation of index hub of hooks

---
+++ src/hooks/index.js
@@ -0,0 +1,16 @@
+export { default as useAdRegistrationHook } from "./useAdRegistrationHook";
+export { default as useAuthenticationHook } from "./useAuthenticationHook";
+export { default as useClientSignupHook } from "./useClientSignupHook";
+export { default as useCustomerEvaluateHook } from "./useCustomerEvaluateHook";
+export { default as useCustomerSelfStatisticsHook } from "./useCustomerSelfStatisticsHook";
+export { default as useJobIconImportHook } from "./useJobIconImportHook";
+export { default as useJobsAdsListDataHook } from "./useJobsAdsListDataHook";
+export { default as useLocationHook } from "./useLocationHook";
+export { default as useMessageFromCustomerToProvider } from "./useMessageFromCustomerToProvider";
+export { default as usePasswordRecoveryChangeHook } from "./usePasswordRecoveryChangeHook";
+export { default as usePasswordRecoveryRequestHook } from "./usePasswordRecoveryRequestHook";
+export { default as useProviderEvaluateHook } from "./useProviderEvaluateHook";
+export { default as useProviderSelfStatisticsHook } from "./useProviderSelfStatisticsHook";
+export { default as useProviderServices } from "./useProviderServices";
+export { default as useProviderSignupHook } from "./useProviderSignupHook";
+export { default as useSignUpFormValidationHook } from "./useSignUpFormValidationHook";
