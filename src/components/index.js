ba3b7efdbcbf8b3536a6e6d7098203953d8c8c0c
Maik Henrique <maik.hsp.contato@gmail.com>
2021-05-07 09:43:54 -0300

    refactor(checkbox): move into the utilities folder

--- src/components/index.js
+++ src/components/index.js
@@ -18,7 +18,7 @@ export { default as JobCardModalHolder } from "./ClassifiedAdsList/JobCardModalH
 export { default as EvaluationAccordion } from "./DashBoard/EvaluationAccordion/EvaluationAccordion";
 export { default as EvaluationAccordionSwitcher } from "./DashBoard/EvaluationAccordion/EvaluationAccordionSwitcher";
 // export { default as Select } from "./Select/Select";
-export { default as Checkbox } from "./Checkbox/Checkbox";
+export { default as Checkbox } from "./Utility/Checkbox/Checkbox";
 export { default as LoadingSpinner } from "./Utility/LoadingSpinner/LoadingSpinner";
 export { default as Button } from "./Utility/Button/Button";
 export { default as Input } from "./Utility/Input/Input";
