a1197ade521756d46dfba0fb1380826499b1e6d1
Maik Henrique <maik.hsp.contato@gmail.com>
2021-05-12 17:48:11 -0300

    refactor(email successfully sent): use of button component utility

--- src/components/ClassifiedAdsList/CustomerMessageToProviderFormModal/EmailSuccessfullySent/EmailSuccessfullySent.jsx
+++ src/components/ClassifiedAdsList/CustomerMessageToProviderFormModal/EmailSuccessfullySent/EmailSuccessfullySent.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import emailSuccessIcon from "../../../../resources/imgs/styleSvgs/email_success.svg";
+
 import styles from "./EmailSuccessfullySent.module.scss";
 import ResponseMessage from "./ResponseMessage/ResponseMessage";
+import { Button } from "../../../index";
+
+import "./Button.scss";
 
 const EmailSuccessfullySent = ({ onClickHandler }) => {
   const { handleModalEvent, handleModalReset } = onClickHandler;
   return (
     <div className={styles.container}>
       <ResponseMessage />
-      <div className={styles.btnContainer}>
-        <button
-          onClick={() => {
-            handleModalEvent();
-            handleModalReset();
-          }}
-          type="button"
-          className={styles.keepSearchingBtn}
-        >
-          Encontrar mais profissionais
-        </button>
-      </div>
+      <Button
+        onClickHandler={() => {
+          handleModalEvent();
+          handleModalReset();
+        }}
+        text="Encontrar mais profissionais"
+        containerClassName="email-successfully-sent-btn-container"
+        buttonClassName="email-successfully-sent-btn"
+      />
     </div>
   );
 };
