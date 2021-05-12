16e692ee891093168d06b732248014a5eae7827d
Maik Henrique <maik.hsp.contato@gmail.com>
2021-05-12 17:33:15 -0300

    refactor(button): addition of children node

--- src/components/Utility/Button/Button.jsx
+++ src/components/Utility/Button/Button.jsx
@@ -7,19 +7,26 @@ const Button = ({
   buttonClassName,
   text,
   type,
+  children,
 }) => (
   <div className={containerClassName}>
     {/* eslint-disable-next-line react/button-has-type */}
     <button type={type} className={buttonClassName} onClick={onClickHandler}>
       {text}
+      {children}
     </button>
   </div>
 );
 
 export default Button;
 
+Button.defaultProps = {
+  children: null,
+};
+
 Button.propTypes = {
   onClickHandler: PropTypes.func.isRequired,
+  children: PropTypes.node,
   containerClassName: PropTypes.string.isRequired,
   buttonClassName: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
